804f3c3bf8f3f00f98bdfb6acb0e0212764d3ee6
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-06-15 20:37:21 +0200

    fix button props, add props type ts file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,12 @@
-import React from 'react'
 import styles from './Header.module.scss';
-import logo from '../img/13-logo.svg';
 import HeaderNav from './HeaderNav';
-
-
 
 export default function Header() {
   return (
     <div>
       <header className={styles.header}>
         <div className={styles.header__logo}>
-            <img src={logo}/>
+            <img src='/src/assets/images/13-logo.svg'/>
         </div>
         <HeaderNav/>
       </header>
