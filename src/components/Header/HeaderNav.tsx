1d1487cba353a1412fbdcbdfd93f6009792f9ca7
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-06-15 20:37:21 +0200

    fix button props, add props type ts file

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -1,35 +1,7 @@
 import { useState } from 'react'
 import styles from './Header.module.scss';
 import MenuIcon from '../img/menu.svg'
-
-interface IHeader  {
-    id: number
-    title: string,
-    route: string
-}
-
-const itemsHeader: IHeader[] = [
- {
-    id: 1,
-    title: 'Мета',
-    route: '/goal'
- },
- {
-    id: 2,
-    title: 'Батальйон',
-    route: '/crew'
- },
- {
-    id: 3,
-    title: 'Як це працює',
-    route: '/how-it-works'
- },
- {
-    id: 4,
-    title: 'Збір',
-    route: '/collection'
- }
-]
+import ITEMS_HEADER from './costants/costants';
 
 export default function HeaderNav() {
     const [isOpen, setIsOpen] = useState(false);
@@ -41,7 +13,7 @@ export default function HeaderNav() {
     <div className={`${styles.header__nav} ${isOpen ? 'is-open' : ''}`}>
         {isOpen && <div className={styles.overlay} onClick={toggleMenu}></div>}
         <nav className={styles.header__nav__desktop} >
-            {itemsHeader.map((item) => (
+            {ITEMS_HEADER.map((item) => (
                 <li key={item.id} className={styles.header__nav__desktop_list} >
                     <a href={item.route} className={styles.header__nav__desktop_list_item}>
                         {item.title}
@@ -59,7 +31,7 @@ export default function HeaderNav() {
             <hr />
 
             <ul className={`${styles.header__mobile_nav} ${isOpen ? styles.is_open : ''}`}>
-                {itemsHeader.map((item) => {
+                {ITEMS_HEADER.map((item) => {
                 return (
                     <li key={item.id} className={styles.header__mobile_nav_li}>
                     <a href={item.route} className={styles.header__mobile_nav_li_a}>
