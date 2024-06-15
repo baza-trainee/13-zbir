54f6d16a91bc19a6f464dbee84ecaa989724bd37
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-06-15 20:37:21 +0200

    fix button props, add props type ts file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Header from "./components/Header/Header";
 import "./styles/index.scss";
 import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
 
 
 const App: FC = () => {
@@ -9,7 +10,7 @@ const App: FC = () => {
     <>
       <Header/>
       <Hero/>
-      <footer>Footer</footer>
+      <Footer/>
     </>
   );
 };
