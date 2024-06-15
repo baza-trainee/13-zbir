005ebdeaeb1cebd584ed5199fc006c6d5667011d
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-06-15 20:37:21 +0200

    fix button props, add props type ts file

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,14 @@
 import styles from './Hero.module.scss'
 import MainImg from '../img/REB.svg'
 import ReusableBtn from '../ReusableBtn/ReusableBtn'
+import heroImg from '../../assets/images/importantInfoSection/soldierDesktop.png'
 
 export default function Hero() {
   return (
     <div className={styles.hero_section} id='goal'>
       <h1 className={styles.hero_section__title}>ЗБІР НА РЕБ ДЛЯ 13-ГО ОКРЕМОГО  СТРІЛЕЦЬКОГО БАТАЛЬЙОНУ</h1>
       <div className={styles.hero_section__main}>
-          <img src={MainImg} className={styles.hero_section__main_img}/>
+          <img src={heroImg} className={styles.hero_section__main_img}/>
           <div className={styles.hero_section__main_text}>
             <h1 className={styles.hero_section__main_text__title}>Наша мета забезпечити 13 ОСБ системою РЕБ</h1>
             <h3 className={styles.hero_section__main_text__subtitle}>90 000 грн</h3>
