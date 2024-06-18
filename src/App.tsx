dcb83c769ab97d432715561bc46c303efbccc574
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-06-18 18:14:12 +0200

    add progress section component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import About from './components/About/About';
 import './styles/index.scss';
 import ImportantInfo from './components/ImportantInfo/ImportantInfo';
 import ScrollUpBtn from './components/ScrollUpBtn/ScrollUpBtn';
+import Progress from './components/Progress/Progress';
 
 const App: FC = () => {
   return (
     <>
       <header className="container">Header</header>
+      <Progress />
       <About />
       <ImportantInfo />
       <ScrollUpBtn />
